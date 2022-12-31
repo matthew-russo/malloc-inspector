504ce9b964c1458bb091b76ecff1005762efefb5
Matthew Russo <matthew@matthewclayrusso.com>
2022-12-31 12:46:45 -0800

    adds func to read a file line by line, reads and prints the exe's memory map via /proc/self/maps

--- src/main.c
+++ src/main.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+#define BUFSIZE 1024
+
+// statically allocate space so we can do some processing on memory maps
+// without using malloc, which may in turn update the memory maps
+char buf_1[BUFSIZE][BUFSIZE];
+char buf_2[BUFSIZE][BUFSIZE];
+
+// reads a file line by line in to a provided buffer
+int read_file_lines(char buf[BUFSIZE][BUFSIZE], char * file_name)
+{
+  FILE * f = fopen(file_name, "r");
+
+  if (f)
+  {
+    int idx = 0;
+    while (fgets(buf[idx], BUFSIZE, f))
+    {
+      idx++;
+    }
+    fclose (f);
+    return idx;
+  }
+  else
+  {
+    printf("File '%s' not present\n", file_name);
+    return -1;
+  }
+}
 
 int main(int argc, char *argv[])
 {
-  printf("hello world\n");
+  int lines_read = read_file_lines(buf_1, "/proc/self/maps");
+  for (int i = 0; i < lines_read; i++)
+  {
+    printf("%s", buf_1[i]);
+  }
   return 0;
 }
